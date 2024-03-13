831d1a9f1f4fa37e50a156908dbda786e0a83352
usersasha18 <x6rrl@yandex.ru>
2024-03-14 00:12:06 +1000

    все хуйня, давай по новой

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,8 @@
+export default function TodoForm() {
+  return (
+        <form className="todo-form">
+            <input type="text" />
+            <button type="submit">Add Task</button>
+        </form>
+  )
+}
---
+++ src/components/TodoWrapper.jsx
@@ -0,0 +1,9 @@
+import TodoForm from "./TodoForm"
+
+export default function TodoWrapper() {
+  return (
+    <div>
+        <TodoForm />
+    </div>
+  )
+}
